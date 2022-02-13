54748bc67ed33421688e2da285d3ec9e85185a99
Anamul haque  shanto <anamulhaque0198877@gmail.com>
2022-02-13 20:34:58 +0600

    sign up page added

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,9 +1,10 @@
 import React from 'react';
-
+import "./Home.css";
 const Dashboard = () => {
     return (
         <React.Fragment>
-     <div className="wrapper">
+          <div className=" register-page">
+          <div className="wrapper">
   <div className="preloader flex-column justify-content-center align-items-center">
     <img className="animation__shake" src="dist/img/AdminLTELogo.png" alt="AdminLTELogo" height={60} width={60} />
   </div>
@@ -592,6 +593,8 @@ const Dashboard = () => {
   <aside className="control-sidebar control-sidebar-dark">
   </aside>
 </div>
+
+          </div>
 
         </React.Fragment>
     );
